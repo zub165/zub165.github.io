8786925663b7226c96df51541583382c46ce6e12
Innovators Generation <zub165@yahoo.com>
2026-08-31 09:53:51 -0400

    Deploy Sadaqaworks Flutter web app under /sadaqaworks/app/.
    HTTPS Zakat API via api.mywaitime.com/sadaqaworks-api

--- sadaqaworks/app/flutter_bootstrap.js
+++ sadaqaworks/app/flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"a804b261645ef8c13eb3d5c44a5c2fb0340c55
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1214923170" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3149059561" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
